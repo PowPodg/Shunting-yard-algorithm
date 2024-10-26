b7e00e4550d9494e35f10b0a5a63009af5ef75d5
PowPodg <oleg@oleg-iv.tk>
2024-10-26 22:28:20 +0500

    create

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Shunting-yard-algorithm
+# Shunting yard algorithm
+
+Shunting yard algorithm - is a method for parsing arithmetical expressions, specified in infix notation.
+
+Conversion of infix expression to postfix expression is implemented - `Infix_to_Postfix(string_view)`.
+
+Two C++ 17 implementations of the algorithm are shown here.<br />
+
+
+$${\color{green} 1 var  }$$<br />
+`string input1 = "(24.5+ 4)*(3.55-1.23)+ 2.89/ 1.44";`  $${\color{green} = 68.127 }$$<br />
+`ShuntingYard sh1;`<br />
+`auto res1 = sh1.Calculation(input1);`<br />
+
+$${\color{green}2 var - without &ensp; brackets &ensp; and &ensp; validation &ensp; checks !!!!!! }$$<br />
+`string input2 = "25.2 + 36.5 * 21 - 43 ";` $${\color{green} = 748.7}$$<br />
+`ShuntingYard_2 sh2;`<br />
+`auto res2 = sh2.Calculation(input2);`
+
---
+++ ShuntingYard.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+class ShuntingYard
+{
+public:
+	vector<string> Parsing_String(string_view);
+	queue<string> Infix_to_Postfix(string_view);
+	double Calcul_Postfix(queue<string>&);
+	double Calculation(string_view);
+private:
+	int Get_Priority(string_view);
+};
+//============================================================================================
+//-----------------------Another variation, only without the brackets!!!!!!----------------------
+
+class ShuntingYard_2
+{
+private:
+	class ICalc {
+	public:
+		double virtual Calculation() = 0;
+	};
+public:
+	vector<string> Parsing_String(string_view);
+	shared_ptr<ICalc> Preparation(vector<string>&);
+	double Calculation(string_view in);
+private:
+	//---------
+	class Value :public ICalc
+	{
+	public:
+		Value(double digit) : _value(digit) {}
+
+		double Calculation()  override {
+			return _value;
+		}
+	private:
+		const double _value;
+	};
+	//-----------
+	class MathOperation : public ICalc
+	{
+	public:
+		MathOperation(string_view value)
+			: priority
+			(
+				[value]()
+				{
+					if (value == "*" || value == "/") return 2;
+					else return 1;
+				}()
+			), math_op(value) {}
+
+				const int priority;
+				double Calculation() override;					
+				void SetLeft(shared_ptr<ICalc> calc) { _left = calc; }
+				void SetRight(shared_ptr<ICalc> calc) { _right = calc; }
+	private:
+		const string math_op;
+		shared_ptr<ICalc> _left, _right;
+	};
+	//----------------------------------------------
+
+};
+
